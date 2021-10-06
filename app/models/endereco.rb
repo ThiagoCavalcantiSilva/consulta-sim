e2657a9683649357f8f9c36757f58f03d0cd67c0
Thiago Cavalcanti Silva <tcavalcanti.tc@gmail.com>
2021-10-05 22:53:09 -0300

    Adicionando validacao de endereco

--- app/models/endereco.rb
+++ app/models/endereco.rb
@@ -1,3 +1,9 @@
 class Endereco < ApplicationRecord
   belongs_to :paciente
+
+  validates :CEP, presence: true, numericality: { only_integer: true }, length: {is: 8}
+  validates :cidade, presence: true, length: {minimum: 2, too_short: "O tamanho minimo deste campo é %{count} caracteres", maximum:20, too_long: "O tamanho máximo deste campo é %{count} caracteres" }
+  validates :bairro, presence: true, length: {minimum: 3, too_short: "O tamanho minimo deste campo é %{count} caracteres ", maximum:50, too_long: "O tamanho máximo deste campo é %{count} caracteres" }
+  validates :logradouro, presence: true, length: {minimum: 3, too_short: "O tamanho minimo deste campo é %{count} caracteres ", maximum:50, too_long: "O tamanho máximo deste campo é %{count} caracteres" }
+  validates :complemento, length: {maximum:50, too_long: "O tamanho máximo deste campo é %{count} caracteres" }
 end
