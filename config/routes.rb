5a0ee2ffb41f9fb7787b2111a7cf5a60841fa261
Thiago Cavalcanti Silva <tcavalcanti.tc@gmail.com>
2021-10-05 22:10:27 -0300

    Adicionando pagina inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :consulta
+  root 'pagina_inicial#index'
+  get 'inicio' => 'pagina_inicial#index'
+
   resources :medicos
-  resources :enderecos
   resources :pacientes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
