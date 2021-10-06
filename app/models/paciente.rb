36a9c70deb6c51a98f12a54a7c22eec2fc830614
Thiago Cavalcanti Silva <tcavalcanti.tc@gmail.com>
2021-10-05 22:43:15 -0300

    Adicionando validacoes no paciente

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -2,4 +2,17 @@ class Paciente < ApplicationRecord
   has_many :enderecos, dependent: :destroy
   accepts_nested_attributes_for :enderecos, allow_destroy: true, reject_if: :all_blank
   has_many :consultum, :dependent => :delete_all
+
+  validates :nomeCompleto, presence: true, length: {minimum: 10, too_short: "O tamanho minimo deste campo é %{count}  caracteres."}
+  validate :validarDataNascimento
+  validates :dataNascimento, presence: true
+  validates :CPF, presence: true, uniqueness: true, numericality: { only_integer: true }, length: {is: 11}
+  validates :email, presence: true, uniqueness: true, length: {minimum: 11, too_short: "O tamanho minimo deste campo é 3 caracteres.", maximum: 40, too_long: "O tamanho máximo deste campo é %{count} caracteres." }
+
+  def validarDataNascimento
+    if dataNascimento.present? && dataNascimento > Date.today
+      errors.add(:dataNascimento, "e uma data invalida!")
+    end
+  end
+
 end
