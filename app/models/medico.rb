39123e1332ef229cdbe16f0e45b6b511706d6e87
Thiago Cavalcanti Silva <tcavalcanti.tc@gmail.com>
2021-10-05 22:47:04 -0300

    Adicionando validacao de medico

--- app/models/medico.rb
+++ app/models/medico.rb
@@ -1,3 +1,18 @@
 class Medico < ApplicationRecord
   has_many :consultum, :dependent => :delete_all
+
+  validates :nomeCompleto, presence: true, length: {minimum: 10, too_short: "O tamanho minimo deste campo é %{count}  caracteres."}
+  validate :validarDataNascimento
+  validates :dataNascimento, presence: true
+  validates :CPF, length: {is: 11}, presence: true, uniqueness: true, numericality: { only_integer: true }
+  validates :email, presence: true
+  validates :especialidade, presence: true
+  validates :CRM, presence: true, uniqueness: true, length: {is: 5}
+
+  def validarDataNascimento
+    if dataNascimento.present? && dataNascimento > Date.today
+      errors.add(:data_nascimento, "é uma data invalida")
+    end
+  end
+
 end
