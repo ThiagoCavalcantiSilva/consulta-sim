05e399b7d24086cec587fceda508f279091cdecf
Thiago Cavalcanti Silva <tcavalcanti.tc@gmail.com>
2021-10-06 19:35:12 -0300

    Adicionando validacoes de consulta

--- app/models/consultum.rb
+++ app/models/consultum.rb
@@ -1,4 +1,12 @@
 class Consultum < ApplicationRecord
   belongs_to :medico
   belongs_to :paciente
+
+  validate :validarData
+
+  def validarData
+    if data.present? && data <= Date.today
+      errors.add(:data, "Data inválida - Marque consultas a partir de amanhã")
+    end
+  end
 end
