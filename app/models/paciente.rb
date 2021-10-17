1b1593ba0a624791132bfc8232f2b6deb02292df
Thiago Cavalcanti Silva <tcavalcanti.tc@gmail.com>
2021-10-16 22:52:50 -0300

    Corrigindo validates de email

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -7,7 +7,7 @@ class Paciente < ApplicationRecord
   validate :validarDataNascimento
   validates :dataNascimento, presence: true
   validates :CPF, presence: true, uniqueness: true, numericality: { only_integer: true }, length: {is: 11}
-  validates :email, presence: true, uniqueness: true, length: {minimum: 6, too_short: "O tamanho minimo deste campo é %{count} caracteres.", maximum: 60, too_long: "O tamanho máximo deste campo é %{count} caracteres." }
+  validates :email, presence: true, uniqueness: true, length: {minimum: 6, too_short: "O tamanho minimo deste campo é %{count} caracteres.", maximum: 60, too_long: "O tamanho máximo deste campo é %{count} caracteres." }, format: { with: URI::MailTo::EMAIL_REGEXP }
 
   def validarDataNascimento
     if dataNascimento.present? && dataNascimento > Date.today
