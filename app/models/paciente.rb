c257d4a5d54be7543441767b79140c9bdfd561e9
Thiago Cavalcanti Silva <tcavalcanti.tc@gmail.com>
2021-10-18 19:57:20 -0300

    Adicionando validacao de endereco no paciente

--- app/models/paciente.rb
+++ app/models/paciente.rb
@@ -9,6 +9,7 @@ class Paciente < ApplicationRecord
   validates :CPF, presence: true, uniqueness: true, numericality: { only_integer: true }, length: {is: 11}
   validates :email, presence: true, uniqueness: true, length: {minimum: 6, too_short: "O tamanho minimo deste campo é %{count} caracteres.", maximum: 60, too_long: "O tamanho máximo deste campo é %{count} caracteres." }, format: { with: URI::MailTo::EMAIL_REGEXP }
 
+  validates :enderecos, presence: true
   def validarDataNascimento
     if dataNascimento.present? && dataNascimento > Date.today
       errors.add(:dataNascimento, "e uma data invalida!")
